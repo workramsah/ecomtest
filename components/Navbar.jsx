768a219c63db9fafedadd74728bb0816c3acfb3c
workramsah <workramsah@gmail.com>
2025-11-28 06:54:02 +0545

    fix nav

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,18 @@
 "use client"
-import React from "react";
+import React, { useState } from "react";
 import { assets} from "@/assets/assets";
 import Link from "next/link"
 import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
 import AuthButton from "@/components/AuthButton";
+import { usePathname } from "next/navigation";
+import LoadingSpinner from "./LoadingSpinner";
 
 const Navbar = () => {
 
   const { isSeller, router, getCartCount } = useAppContext();
+    const [loading,setLoading]=useState(false);
+  const pathname = usePathname();
 
   return (
     <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
@@ -19,10 +23,10 @@ const Navbar = () => {
         alt="logo"
       />
       <div className="flex items-center gap-4 lg:gap-8 max-md:hidden">
-        <Link href="/" className="hover:text-gray-900 transition">
+        <Link href="/" className={pathname === "/" ? "text-orange-500 font-bold" : "hover:text-gray-900 transition"}>
           Home
         </Link>
-        <Link href="/all-products" className="hover:text-gray-900 transition">
+        <Link href="/all-products" className={pathname === "/all-products" ? "text-orange-500 font-bold" : "hover:text-gray-900 transition"}>
           Shop
         </Link>
         <Link href="/" className="hover:text-gray-900 transition">
@@ -38,14 +42,16 @@ const Navbar = () => {
 
       <ul className="hidden md:flex items-center gap-4">
         <Image className="w-4 h-4" src={assets.search_icon} alt="search icon" />
-        <div className="relative cursor-pointer" onClick={() => router.push('/cart')}>
-          <Image className="w-4 h-4" src={assets.cart_icon} alt="cart icon" />
+       <Link onClick={()=>setLoading(true)} href="/cart">
+        <div className="relative cursor-pointer" >
+          {loading ? <LoadingSpinner/> :<Image className="w-4 h-4" src={assets.cart_icon} alt="cart icon" />}
           {getCartCount() > 0 && (
             <span className="absolute -top-2 -right-2 bg-orange-500 text-white text-xs rounded-full w-4 h-4 flex items-center justify-center">
               {getCartCount()}
             </span>
           )}
         </div>
+        </Link>
         <AuthButton />
       </ul>
 
